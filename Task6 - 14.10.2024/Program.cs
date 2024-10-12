49de84c0471ca0d5cd98073ea34063b81bae4399
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-10-12 17:43:15 +0400

    Initial

---
+++ Task6 - 14.10.2024/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task6___14._10._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Nicat", "Ibrahimli", "BP217", 91);
+
+            #region UserInputedVersion
+            //Console.Write("Enter student's name: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Enter student's surname: ");
+            //string surname = Console.ReadLine();
+
+            //Console.Write("Enter student's group: ");
+            //string group = Console.ReadLine();
+
+            //Console.Write("Enter student's point (0-100): ");
+            //int point = int.Parse(Console.ReadLine());
+
+            //Student student1 = new Student(name, surname, group, point); 
+            #endregion
+
+            student1.GetInfo();
+            student1.CheckGraduatiob();
+            student1.GetDiplomDegree();
+        }
+    }
+}
---
+++ Task6 - 14.10.2024/Student.cs
@@ -0,0 +1,65 @@
+﻿namespace Task6___14._10._2024
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+
+        public Student(string name, string surname, string group, int point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+            if (point < 65)
+            {
+                IsGraduated = false;
+            }
+            else
+            {
+                IsGraduated = true;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nSurname: {Surname}\nGroup: {Group}\nPoint: {Point}\nGraduated: {IsGraduated}");
+        }
+
+        public void CheckGraduatiob()
+        {
+            if (IsGraduated == false)
+            {
+                Console.WriteLine($"{Name} did not graduate.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} graduated.");
+            }
+        }
+
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine($"{Name} did not get any diploma.");
+            }
+            else if (Point >= 65 && Point < 80)
+            {
+                Console.WriteLine($"{Name} got Ordinary diploma.");
+            }
+            else if (Point >= 80 && Point < 90)
+            {
+                Console.WriteLine($"{Name} got Honor diploma.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} got High Honor diploma.");
+            }
+        }
+    }
+}
